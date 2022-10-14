e94798417c6adc1786e70ddd5e2f9c52d193a665
tien.le <tien.le@kyanon.digital>
2022-10-14 17:10:15 +0700

    update

--- App.js
+++ App.js
@@ -4,33 +4,56 @@ import { Text, View, TouchableOpacity, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function FirstScreen() {
-  const [selectedValue, setSelectedValue] = useState('ltr');
-  const values = ['ltr', 'rtl'];
+const FirstScreen = () => {
+  const [direction, setDirection] = useState('flex-start');
+
   return (
-    <View style={{ padding: 10, flex: 1 }}>
-      <View style={styles.row}>
-        {values.map(value => (
-          <TouchableOpacity key={value}
-            onPress={() => setSelectedValue(value)}
-            style={[styles.button, selectedValue === value && styles.selected]}>
-            <Text style={[
+    <PreviewLayout
+      label="direction"
+      selectedValue={direction}
+      values={['flex-start', 'flex-end']}
+      setSelectedValue={setDirection}>
+      <View style={[styles.box, {backgroundColor: '#EE2C38'}]} />
+      <View style={[styles.box, {backgroundColor: '#FAA030'}]} />
+      <View style={[styles.box, {backgroundColor: '#32B76C'}]} />
+    </PreviewLayout>
+  );
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{padding: 10, flex: 1}}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map(value => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}>
+          <Text
+            style={[
               styles.buttonLabel,
               selectedValue === value && styles.selectedLabel,
             ]}>
-              {value}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-      <View style={[styles.container, { direction: selectedValue }]}>
-        <View style={{ width: 50, height: 50, backgroundColor: '#EE2C38' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: '#FAA030' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: '#32B76C' }} />
-      </View>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
     </View>
-  )
-}
+    <View
+      style={[
+        styles.container,
+        {alignItems: selectedValue},
+      ]}>
+      {children}
+    </View>
+  </View>
+);
 
 
 function SecondScreen() {
