83a1244d5d15663ec557cbd4e6dc65508c00136c
tien.le <tien.le@kyanon.digital>
2022-10-18 16:49:45 +0700

    banner figma

--- App.js
+++ App.js
@@ -1,243 +1,86 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import React, { useState } from 'react';
+// import { StyleSheet, Text, View,Image,ImageBackground, TouchableOpacity, } from 'react-native';
 
-const FirstScreen = () => {
-  const [direction, setDirection] = useState('flex-start');
 
-  return (
-    <PreviewLayout
-      label="direction"
-      selectedValue={direction}
-      values={['flex-start', 'flex-end']}
-      setSelectedValue={setDirection}>
-      <View style={[styles.box, {backgroundColor: '#EE2C38'}]} />
-      <View style={[styles.box, {backgroundColor: '#FAA030'}]} />
-      <View style={[styles.box, {backgroundColor: '#32B76C'}]} />
-    </PreviewLayout>
-  );
-};
+// function App() {
+//   return ( <View>
+//     <Product/>
+//     <ImageBackground
+//         style={styles.stretch}
+//         source={require('./images/banner.png')}
+//       />
+//     <Text>aaa</Text>
+//   </View> );
+// }
 
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => (
-  <View style={{padding: 10, flex: 1}}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}>
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}>
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
+// export default App;
+// const Product = () => {
+//   return (
+//       <View style={styles.product}>
+//           <Text>aaaaa</Text>
+//       </View>
+//   )
+// }
+
+
+// const styles = StyleSheet.create({
+//   product: {
+//       backgroundColor: "skyblue",
+//       width: 100,
+//       height: 200,
+//   }
+// })
+
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const App = () => (
+  <View style={styles.container}>
+    <View style={styles.banner}>
+      <ImageBackground source={require('./images/banner.png')} resizeMode="cover" style={styles.image}>
+
+        <View style={styles.banner_title}>
+          <Text style={styles.text_title}>Save extra on every order</Text>
+          <Text style={styles.bottom_title}>Etiam mollis metus non faucibus sollicitudin.</Text>
+        </View>
+      </ImageBackground>
     </View>
-    <View
-      style={[
-        styles.container,
-        {alignItems: selectedValue},
-      ]}>
-      {children}
-    </View>
+
   </View>
 );
 
-
-function SecondScreen() {
-  const [position, setPosition] = useState('relative');
-  const values = ['relative', 'absolute'];
-
-  return (
-    <View style={{ padding: 10, flex: 1 }}>
-      <View style={styles.row}>
-        {values.map(value => (
-          <TouchableOpacity
-            key={value}
-            onPress={() => setPosition(value)}
-            style={[styles.button, position === value && styles.selected]}>
-            <Text
-              style={[
-                styles.buttonLabel,
-                position === value && styles.selectedLabel,
-              ]}>
-              {value}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-      <View style={styles.container}>
-        <View
-          style={[
-            styles.box,
-            {
-              top: 25,
-              left: 25,
-              position,
-              backgroundColor: '#EE2C38',
-            },
-          ]}
-        />
-        <View
-          style={[
-            styles.box,
-            {
-              top: 50,
-              left: 50,
-              position,
-              backgroundColor: '#FAA030',
-            },
-          ]}
-        />
-        <View
-          style={[
-            styles.box,
-            {
-              top: 75,
-              left: 75,
-              position,
-              backgroundColor: '#32B76C',
-            },
-          ]}
-        />
-      </View>
-    </View>
-  );
-}
-
-function ThirdScreen() {
-  const [justifyContent, setJustifyContent] = useState("flex-start");
-
-  return (
-    <PreviewLayout3
-      label="justifyContent"
-      selectedValue={justifyContent}
-      values={[
-        "flex-start",
-        "flex-end",
-        "center",
-        "space-between",
-        "space-around",
-        "space-evenly",
-      ]}
-      setSelectedValue={setJustifyContent}
-    >
-      <View
-        style={[styles.box, { backgroundColor: "#EE2C38" }]}
-      />
-      <View
-        style={[styles.box, { backgroundColor: "#FAA030" }]}
-      />
-      <View
-        style={[styles.box, { backgroundColor: "#32B76C" }]}
-      />
-    </PreviewLayout3>
-  );
-}
-const PreviewLayout3 = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => (
-  <View style={{ padding: 10, flex: 1 }}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map((value) => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}
-        >
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}
-          >
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={[styles.container, { [label]: selectedValue }]}>
-      {children}
-    </View>
-  </View>
-);
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="direction" component={FirstScreen} />
-      <Tab.Screen name="position" component={SecondScreen} />
-      <Tab.Screen name="justifyContene" component={ThirdScreen} />
-    </Tab.Navigator>
-  );
-}
-
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 8,
-    backgroundColor: 'grey',
   },
-  box: {
-    width: 50,
-    height: 50,
+  banner: {
+    margin: 20,
+    marginRight: 20,
+    width: '90%',
+    height: 200,
   },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+  banner_title: {
+    padding: 25,
+    width: "90%",
+    height: "100%"
   },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'oldlace',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
+  image: {
+    borderRadius: 5,
+    flex: 1,
   },
-  selected: {
-    backgroundColor: 'coral',
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'coral',
-  },
-  selectedLabel: {
-    color: 'white',
-  },
-  label: {
-    textAlign: 'center',
-    marginBottom: 10,
-    fontSize: 24,
-  },
-};
+  bottom_title: {
+    paddingTop: 15,
+    fontSize: 18,
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
-}
+  }
+  ,
+  text_title: {
+    width: '50%',
+    color: "#1987FB",
+    fontSize: 23,
+    lineHeight: 25,
+    fontWeight: "bold",
+  }
+});
+
+export default App;
