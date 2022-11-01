37417f34f856f0ad2137371d8aefa0d1ed8f41ea
tien.le <tien.le@kyanon.digital>
2022-11-01 14:36:37 +0700

    update

---
+++ src/containers/Containers.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Banner from '../components/banner/Banner'
+
+const Containers = () => {
+  return (
+    <View>
+       <Banner/>
+    </View>
+  )
+}
+
+export default Containers
